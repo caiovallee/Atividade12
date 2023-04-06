99da9255d966a04fec4aa0cb8edfa69aef98c376
caiovallee <caiohgvalle.14@gmail.com>
2023-04-06 13:23:45 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade12
+{
+    internal class Program
+    {
+// Dados três valores X, Y, Z, verifiquem se eles podem ser os comprimentos dos lados de um triângulo
+// e se forem escrever uma mensagem informando se é um triângulo eqüilátero, isósceles ou escaleno.
+//Observações:
+//O comprimento de um lado do triângulo é sempre menor do que a soma dos outros dois.
+//•Eqüilátero: Todos os lados iguais
+//•Isósceles: Dois lados iguais
+//•Escaleno: Todos os lados diferentes
+
+        static void Main(string[] args)
+        {
+            traingulo triangulo = new traingulo();
+            Console.WriteLine("Digite o valor de X");
+            triangulo.x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de y");
+            triangulo.y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de z");
+            triangulo.z = int.Parse(Console.ReadLine());
+            Console.WriteLine(triangulo.ReceberTexto());
+            Console.ReadLine();
+            
+
+
+        }
+    }
+}
---
+++ Atividade12/traingulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade12
+{
+    internal class traingulo
+    {
+        internal int y;
+        internal int z;
+        internal int x;
+
+        internal string ReceberTexto()
+        {
+            string texto = "";
+            if (x == y && x == z)
+            {
+                texto = "O triangulo é equilatero";
+            }
+            else
+            {
+                texto = "O triangulo não é equilatero";
+            }
+            return texto;
+        }
+    }
+}
